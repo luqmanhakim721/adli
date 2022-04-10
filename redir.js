977fd6ffe2e350decdf7b0b6a74212415f1619d2
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2022-04-10 20:28:16 +0700

    dser

--- redir.js
+++ redir.js
@@ -3,3 +3,4 @@ var d= window.location.href;
 d=d.replace("?", " ");
 d=d.match(/\d{11,20}/);
 location.href = 'http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/'+d+'.html&aff_short_key=xxxxx';
+XX
